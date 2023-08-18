c2fe87db7e33edb6ef884747c17d542b6c7d1c0b
Nawaz <mnawaz6935@gmail.com>
2023-08-18 20:01:53 +0500

    added slash

--- upload.py
+++ upload.py
@@ -1,5 +1,7 @@
 import os
 import requests
+from tqdm import tqdm
+from pathlib import Path
 
 FILE_PATH = r'e:\Dashboard_comlpeted_v1\Dashboard (2).zip'
 ENDPOINT_URL = 'https://3.135.156.101:8091/upload_file/'
@@ -8,22 +10,23 @@ def upload_with_progress(file_path, url):
     print(f'Files Exists: {os.path.exists(file_path)}')
     total_size = os.path.getsize(file_path)
     print(f'File Size: {total_size}')
-    uploaded_size = 0
-    headers = {'Content-Type': 'multipart/form-data'}
-    with open(file_path, 'rb', encoding='utf-8') as f:
-        with requests.post(url, data=f, headers=headers, stream=True, verify=False) as response:
-            try:
-                response.raise_for_status()
-                for chunk in response.iter_content(chunk_size=8192):
-                    uploaded_size += len(chunk)
-                    percent_complete = (uploaded_size / total_size) * 100
-                    print(f"Uploading: {percent_complete:.2f}% complete", end='\r', flush=True)
+    # Get the file size for progress tracking
+    file_size = Path(file_path).stat().st_size
 
-                print("Upload completed.")
-            except requests.exceptions.HTTPError as e:
-                print("Upload failed. HTTP Error:")
-                print(e)
-                print(response.text)
+    with open(file_path, 'rb') as file:
+        files = {
+            'file': (file_path, file),
+            'FileName': (None, 'dashboard.zip')
+        }
+
+        with tqdm(total=file_size, unit='B', unit_scale=True, unit_divisor=1024) as pbar:
+            def update_progress(bytes_amount):
+                pbar.update(bytes_amount - pbar.n)
+
+            response2 = requests.post(url, files=files, verify=False, hooks={'response': update_progress})
+
+            if response2.status_code == 200:
+                print('File uploaded successfully')
     print("Upload completed.")
 
 if __name__ == "__main__":
